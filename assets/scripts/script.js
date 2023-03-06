d6c9d0fb4fe72e1727d1b45957933568a4f95ff6
wellyngton souza <wellyngtonmonteiro84@gmail.com>
2023-03-06 19:50:51 -0300

    update projeto

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -12,39 +12,42 @@ sub_menus.forEach(function (menus) {
 
             if (!isExpanded) {
                 setTimeout(() => {
+                    
                     sub_menu.style.visibility = "visible"
                     icon.style.transform = "rotate(180deg)"
                     resolve()
                 }, 100)
-
-
-            } else {
-
+            }else{
                 sub_menu.style.opacity = "0"
-
+                
                 setTimeout(() => {
                     icon.style.transform = "rotate(0deg)"
                     reject()
-                }, 300)
+                }, 500)
+                
             }
-
-
         })
-
         promise.then(() => {
             sub_menu.style.opacity = "1"
         }).catch(() => {
             sub_menu.style.visibility = "hidden"
+            
         })
-
-
         sub_menu.dataset.show = !isExpanded;
     })
 })
 
 let categorias = [
     {
-        marca: "Avon",
+        marca: "avon",
+        sub_categorias: ["perfumes", "hidradantes", "maquiagem", "kids"],
+        imagens: [
+            ["p01.jpg", "p02.jpg", "p03.jpg", "p04.jpg"], ["hidratantes1", "hidratantes2", "hidratantes3", "hidratantes4"], ["maquiagem1", "maquiagem2", "maquiagem3", "maquiagem4"], ["kids1", "kids2", "kids3", "kids4"]
+        ],
+        precos: ["valor", 'valor', "valor", "valor"],
+    },
+    {
+        marca: "natura",
         sub_categorias: ["perfumes", "hidradantes", "maquiagem", "kids"],
         imagens: [
             ["tal", "tal", "tal", "tal"], ["tal", "tal", "tal", "tal"], ["tal", "tal", "tal", "tal"], ["tal", "tal", "tal", "tal"]
@@ -52,7 +55,7 @@ let categorias = [
         precos: ["valor", 'valor', "valor", "valor"],
     },
     {
-        marca: "Natura",
+        marca: "boticario",
         sub_categorias: ["perfumes", "hidradantes", "maquiagem", "kids"],
         imagens: [
             ["tal", "tal", "tal", "tal"], ["tal", "tal", "tal", "tal"], ["tal", "tal", "tal", "tal"], ["tal", "tal", "tal", "tal"]
@@ -60,15 +63,7 @@ let categorias = [
         precos: ["valor", 'valor', "valor", "valor"],
     },
     {
-        marca: "Boticario",
-        sub_categorias: ["perfumes", "hidradantes", "maquiagem", "kids"],
-        imagens: [
-            ["tal", "tal", "tal", "tal"], ["tal", "tal", "tal", "tal"], ["tal", "tal", "tal", "tal"], ["tal", "tal", "tal", "tal"]
-        ],
-        precos: ["valor", 'valor', "valor", "valor"],
-    },
-    {
-        marca: "Romance",
+        marca: "romance",
         sub_categorias: ["calcinhas", "sutiãs", "calças", "calças legins"],
         imagens: [
             ["tal", "tal", "tal", "tal"], ["tal", "tal", "tal", "tal"], ["tal", "tal", "tal", "tal"], ["tal", "tal", "tal", "tal"]
@@ -77,18 +72,17 @@ let categorias = [
     },
 ]
 
-
-
-
 let selected = document.querySelector(".titulo select")
 
 let categorias_produtos = document.querySelectorAll(".titulo_produtos > ul > button")
 let fotos = document.querySelectorAll(".cards_produtos > ul > li > img")
+
+
 let sumario_produtos = document.querySelectorAll(".cards_produtos > ul > li > div > h2 > span")
 selected.addEventListener("change", function (element) {
     let option = element.target.value
 
-
+    
     for (let index = 0; index < categorias_produtos.length; index++) {
         if (option == categorias[index].marca) {
             for (let dis = 0; dis < categorias_produtos.length; dis++) {
@@ -99,23 +93,25 @@ selected.addEventListener("change", function (element) {
 
 })
 
-categorias_produtos.forEach((element) => {
+categorias_produtos.forEach((element, ind) => {
     element.addEventListener("click", function () {
+
+        categorias_produtos.forEach(elements => elements.classList.remove("ativos"))
+
+        this.classList.add("ativos")
+        let exist = this.classList.contains("ativos")
+        let option = selected.value
+        if (exist) {
+            
+        }
+
         
-        categorias_produtos.forEach(elements=>elements.dataset.clickin = false)
-
-        let controle = this.dataset.clickin
-        this.dataset.clickin = !!controle
-
-        categorias_produtos.forEach((elements)=>{
-
-            if(elements.dataset.clickin == "true"){
-                elements.style.backgroundColor = "rgba(255,255,255, 0.1)"
+        for (let index = 0; index < categorias_produtos.length; index++) {
+                if (categorias[index].marca == option && categorias[index].sub_categorias[ind] == this.value ) {
+                    for (let dis = 0; dis < categorias[index].imagens.length; dis++) {
+                        fotos[dis].src = `./assets/img/listFotos/${option}/${this.value}/${categorias[index].imagens[ind][dis]}`
+                    }
+                }
             }
-            else{
-                elements.style.backgroundColor = "rgba(255,255,255, 1)"
-            }
-        })
-        
     })
 })
